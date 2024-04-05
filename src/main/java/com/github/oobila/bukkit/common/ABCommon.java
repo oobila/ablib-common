5b6d8055e87b11281cdc236c6543a64f0fd30e5b
BuildTools <unconfigured@null.spigotmc.org>
2024-04-05 01:10:24 +0100

    updating register

--- src/main/java/com/github/oobila/bukkit/common/ABCommon.java
+++ src/main/java/com/github/oobila/bukkit/common/ABCommon.java
@@ -3,24 +3,29 @@ package com.github.oobila.bukkit.common;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredListener;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.logging.Level;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ABCommon {
 
-    private static final Set<Listener> LISTENERS = new HashSet<>();
-
     public static void register(Listener listener, Plugin plugin) {
-        if (!LISTENERS.contains(listener)) {
-            LISTENERS.add(listener);
-            Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
-            Bukkit.getLogger().log(Level.INFO, "Registering listener {0} with plugin {1}", new String[]{listener.getClass().getSimpleName(), plugin.getName()});
+        for (Plugin p : Bukkit.getServer().getPluginManager().getPlugins()) {
+            for (RegisteredListener registeredListener : HandlerList.getRegisteredListeners(p)) {
+                if (registeredListener.getListener().equals(listener)) {
+                    Bukkit.getLogger().log(Level.INFO, "Not Registering listener {0} with plugin {1} as it has already been registered",
+                            new String[]{listener.getClass().getSimpleName(), plugin.getName()});
+                    return;
+                }
+            }
         }
+
+        Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
+        Bukkit.getLogger().log(Level.INFO, "Registering listener {0} with plugin {1}", new String[]{listener.getClass().getSimpleName(), plugin.getName()});
     }
 
 }
