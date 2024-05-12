98df682f5420829697d42f72d8083646e423f364
BuildTools <unconfigured@null.spigotmc.org>
2024-05-12 14:50:55 +0100

    removing ABCore references

--- src/main/java/com/github/oobila/bukkit/common/ABCommon.java
+++ src/main/java/com/github/oobila/bukkit/common/ABCommon.java
@@ -17,31 +17,41 @@ public class ABCommon {
     private static final String ABCORE_PLUGIN_NAME = "ABCore";
     private static final String NAMESPACE = "ab_common";
 
-    public static <T> void register(T object, Plugin plugin) throws CannotFindABCoreException, ServerContextException {
-        //only used for registering listeners
-        Class<?> type = object.getClass();
-        if (Listener.class.isAssignableFrom(type)) {
-            Bukkit.getPluginManager().registerEvents((Listener) object, plugin);
-            ServerContext.fromABCore().store(Listener.class, object.getClass().getName(), object);
-        } else {
-            ServerContext.fromABCore().store(type, object.getClass().getName(), object);
-        }
+    public static <T> void register(T object, Plugin plugin) {
+        handleABCoreException(() -> {
+            //only used for registering listeners
+            Class<?> type = object.getClass();
+            if (Listener.class.isAssignableFrom(type)) {
+                Bukkit.getPluginManager().registerEvents((Listener) object, plugin);
+                ServerContext.fromABCore().store(Listener.class, object.getClass().getName(), object);
+            } else {
+                ServerContext.fromABCore().store(type, object.getClass().getName(), object);
+            }
+        });
     }
 
-    public static BukkitTask runTask(Runnable task) throws CannotFindABCoreException {
-        return Bukkit.getScheduler().runTask(getABCore(), task);
+    public static BukkitTask runTask(Runnable task) {
+        return handleABCoreException(() ->
+                Bukkit.getScheduler().runTask(getABCore(), task)
+        );
     }
 
-    public static BukkitTask runTaskAsync(Runnable task) throws CannotFindABCoreException {
-        return Bukkit.getScheduler().runTaskAsynchronously(getABCore(), task);
+    public static BukkitTask runTaskAsync(Runnable task) {
+        return handleABCoreException(() ->
+                Bukkit.getScheduler().runTaskAsynchronously(getABCore(), task)
+        );
     }
 
-    public static BukkitTask runTaskLater(Runnable task, long ticks) throws CannotFindABCoreException {
-        return Bukkit.getScheduler().runTaskLater(getABCore(), task, ticks);
+    public static BukkitTask runTaskLater(Runnable task, long ticks) {
+        return handleABCoreException(() ->
+                Bukkit.getScheduler().runTaskLater(getABCore(), task, ticks)
+        );
     }
 
-    public static BukkitTask runContinuousTask(Runnable task, long ticks) throws CannotFindABCoreException {
-        return Bukkit.getScheduler().runTaskTimer(getABCore(), task, ticks, ticks);
+    public static BukkitTask runContinuousTask(Runnable task, long ticks) {
+        return handleABCoreException(() ->
+                Bukkit.getScheduler().runTaskTimer(getABCore(), task, ticks, ticks)
+        );
     }
 
     public static void cancelTask(int taskId) {
@@ -61,15 +71,17 @@ public class ABCommon {
         return new NamespacedKey(NAMESPACE, name);
     }
 
-    public static void checkRequiredVersion(Plugin plugin, String requiredCoreVersion) throws CannotFindABCoreException {
-        if(requiredCoreVersion != null && !requiredCoreVersion.isEmpty()){
-            Plugin abCore = getABCore();
-            Version coreVersion = new Version(abCore.getDescription().getVersion());
-            if(coreVersion.compareTo(new Version(requiredCoreVersion)) < 0){
-                log(Level.SEVERE, "This version of {0} plugin requires ABCore v{1}", plugin.getName(), requiredCoreVersion);
-                Bukkit.shutdown();
+    public static void checkRequiredVersion(Plugin plugin, String requiredCoreVersion) {
+        handleABCoreException(() -> {
+            if(requiredCoreVersion != null && !requiredCoreVersion.isEmpty()){
+                Plugin abCore = getABCore();
+                Version coreVersion = new Version(abCore.getDescription().getVersion());
+                if(coreVersion.compareTo(new Version(requiredCoreVersion)) < 0){
+                    log(Level.SEVERE, "This version of {0} plugin requires ABCore v{1}", plugin.getName(), requiredCoreVersion);
+                    Bukkit.shutdown();
+                }
             }
-        }
+        });
     }
 
     public static void checkForUpdate(Plugin plugin, int spigotId) {
@@ -84,6 +96,33 @@ public class ABCommon {
             throw new CannotFindABCoreException();
         }
         return plugin;
+    }
+
+    private static <T> T handleABCoreException(ABCoreRunnable<T> runnable) {
+        try {
+            return runnable.run();
+        } catch (Exception e) {
+            log(Level.SEVERE, e);
+            Bukkit.shutdown();
+            return null;
+        }
+    }
+
+    private static void handleABCoreException(ABCoreVoidRunnable runnable) {
+        try {
+            runnable.run();
+        } catch (Exception e) {
+            log(Level.SEVERE, e);
+            Bukkit.shutdown();
+        }
+    }
+
+    private interface ABCoreRunnable<T> {
+        T run() throws CannotFindABCoreException, ServerContextException;
+    }
+
+    private interface ABCoreVoidRunnable {
+        void run() throws CannotFindABCoreException, ServerContextException;
     }
 
 }
